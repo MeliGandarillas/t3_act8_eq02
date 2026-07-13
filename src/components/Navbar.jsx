31b4ace27aa59c531ddbd29562a611a16ad5a2a4
MeliGandarillas <gandarillasmelissa@gmail.com>
2026-07-12 20:48:56 -0600

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import "../styles/layout.css";
+
+function Navbar({ usuarioActivo, onLogout }) {
+  return (
+    <div className="navbar">
+      <div className="navbar-brand">
+        <span className="navbar-title">Manos de Oaxaca</span>
+      </div>
+      <div className="navbar-user">
+        <span className="navbar-greeting">Hola, {usuarioActivo.firstName}!</span>
+        <img
+          src={usuarioActivo.image}
+          alt="Foto de perfil"
+          className="navbar-avatar"
+        />
+        <button className="navbar-logout" onClick={onLogout}>
+          Cerrar sesión
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
