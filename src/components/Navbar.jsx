17871dfc0b0d40ff3c7fb851e2175d47542b208a
MeliGandarillas <gandarillasmelissa@gmail.com>
2026-07-14 10:32:11 -0600

    Actualizamos componente navbar para incluir logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,41 @@
+import icono from "../assets/icono.png";
 import "../styles/layout.css";
 
-function Navbar({usuarioActivo, onLogout }) {
+function Navbar({ usuarioActivo, onLogout }) {
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <div className="navbar-brand">
-        <span className="navbar-title">Manos de Oaxaca</span>
+        <img
+          src={icono}
+          alt="Logo de Manos de Oaxaca"
+          className="navbar-logo"
+        />
+
+        <span className="navbar-title">
+          Manos de Oaxaca
+        </span>
       </div>
+
       <div className="navbar-user">
-        <span className="navbar-greeting">Hola, {usuarioActivo.firstName}!</span>
+        <span className="navbar-greeting">
+          Hola, {usuarioActivo.firstName}!
+        </span>
+
         <img
           src={usuarioActivo.image}
           alt="Foto de perfil"
           className="navbar-avatar"
         />
-        <button className="navbar-logout" onClick={onLogout}>
+
+        <button
+          type="button"
+          className="navbar-logout"
+          onClick={onLogout}
+        >
           Cerrar sesión
         </button>
       </div>
-    </div>
+    </nav>
   );
 }
 
